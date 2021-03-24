981236d1290c6b27161e51df16dfb091e3fd59dc
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-03-24 01:42:29 +0100

    challenge done

---
+++ countE.js
@@ -0,0 +1,55 @@
+/*
+countE.js
+Ecrivez un programme qui compte le nombre de e dans le texte de l'exercice nbChar.js
+*/
+
+let text =
+  "Je suis le ténébreux, - le veuf, - l'inconsolé,\n\
+Le prince d'Aquitaine à la tour abolie :\n\
+Ma seule étoile est morte, - et mon luth constellé\n\
+Porte le soleil noir de la Mélancolie.\n\
+\n\
+Dans la nuit du tombeau, toi qui m'as consolé,\n\
+Rends-moi le Pausilippe et la mer d'Italie,\n\
+La fleur qui plaisait tant à mon cœur désolé,\n\
+Et la treille où le pampre à la rose s'allie.\n\
+\n\
+Suis-je Amour ou Phébus ? ... Lusignan ou Biron ?\n\
+Mon front est rouge encor du baiser de la reine ;\n\
+J'ai rêvé dans la grotte où nage la sirène...\n\
+\n\
+Et j'ai deux fois vainqueur traversé l'Achéron ;\n\
+Modulant tour à tour sur la lyre d'Orphée\n\
+Les soupirs de la sainte et les cris de la fée.";
+
+// C'est la version qui se rapproche le plus du nombre de "e" dans le texte
+
+const count = [...text].filter((l) => l === "e" || l === "é" || l === "è" || l === "E" || l === "ê").length;
+console.log(`There is ${count} "e" in the text`);
+
+console.log(text);
+
+/*
+
+OUTPUT :
+
+There is 77 "e" in the text
+
+Le prince d'Aquitaine à la tour abolie :
+Ma seule étoile est morte, - et mon luth constellé
+Porte le soleil noir de la Mélancolie.
+
+Dans la nuit du tombeau, toi qui m'as consolé,
+Rends-moi le Pausilippe et la mer d'Italie,
+La fleur qui plaisait tant à mon cœur désolé,
+Et la treille où le pampre à la rose s'allie.
+
+Suis-je Amour ou Phébus ? ... Lusignan ou Biron ?
+Mon front est rouge encor du baiser de la reine ;
+J'ai rêvé dans la grotte où nage la sirène...
+
+Et j'ai deux fois vainqueur traversé l'Achéron ;
+Modulant tour à tour sur la lyre d'Orphée
+Les soupirs de la sainte et les cris de la fée.
+
+*/
